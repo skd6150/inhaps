d3d7aaf61aad14dd05fb4c211094f6e640eac16c
skd6150 <skd6150@gmail.com>
2022-05-14 00:38:19 +0900

    feat: header, footer 수정

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,15 @@
+import style from "../styles/footer.module.css";
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer className={style.footerWrapper}>
+      <span className={style.author}>
+        © {new Date().getFullYear()} powered by{" "}
+        <a href="https://github.com/caputdraconis050630">caputdraconis</a>,{" "}
+        <a href="https://github.com/skd6150">skd6150</a>
+      </span>
+    </footer>
+  );
 };
 
 export default Footer;
--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,42 @@
+import { useState, useEffect } from "react";
+import { throttle } from "lodash";
+import styles from "../styles/header.module.css";
+
+const getWindowWidth = () => {
+  const { innerWidth, innerHeight } = window;
+  return { innerWidth, innerHeight };
+};
+
 const Header = () => {
-  return <header>header</header>;
+  const [width, setWidth] = useState<number>(0);
+  const [headerHeight, setHeaderHeight] = useState(0);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(getWindowWidth().innerWidth);
+      // setHeaderHeight(getWindowWidth().innerWidth > 375 ? 230 : 150);
+      setHeaderHeight(100);
+    };
+    handleResize();
+    const throttledHandleResize = throttle(handleResize, 50);
+    window.addEventListener("resize", throttledHandleResize);
+    return () => window.removeEventListener("resize", throttledHandleResize);
+  }, []);
+
+  return (
+    <header className={styles.headerWrapper}>
+      <div
+        className={styles.header}
+        style={{
+          clipPath: `path("M0,0 V ${headerHeight} Q${width / 2},${
+            headerHeight + 80
+          } ${width},${headerHeight} V -${headerHeight}")`,
+        }}
+      >
+        <h1 className={styles.title}>Lorem Ipsum</h1>
+      </div>
+    </header>
+  );
 };
 
 export default Header;

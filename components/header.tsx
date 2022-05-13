dd104ad16f4afaaeaddd1d19699fc63c025a4e31
skd6150 <skd6150@gmail.com>
2022-05-14 00:47:31 +0900

    refactor: 불필요한 변수 제거

--- components/header.tsx
+++ components/header.tsx
@@ -9,13 +9,10 @@ const getWindowWidth = () => {
 
 const Header = () => {
   const [width, setWidth] = useState<number>(0);
-  const [headerHeight, setHeaderHeight] = useState(0);
 
   useEffect(() => {
     const handleResize = () => {
       setWidth(getWindowWidth().innerWidth);
-      // setHeaderHeight(getWindowWidth().innerWidth > 375 ? 230 : 150);
-      setHeaderHeight(100);
     };
     handleResize();
     const throttledHandleResize = throttle(handleResize, 50);
@@ -28,9 +25,7 @@ const Header = () => {
       <div
         className={styles.header}
         style={{
-          clipPath: `path("M0,0 V ${headerHeight} Q${width / 2},${
-            headerHeight + 80
-          } ${width},${headerHeight} V -${headerHeight}")`,
+          clipPath: `path("M0,0 V 100 Q${width / 2},180 ${width},100 V -100")`,
         }}
       >
         <h1 className={styles.title}>Lorem Ipsum</h1>

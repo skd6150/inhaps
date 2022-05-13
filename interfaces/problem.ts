280dc41d0cc15d6e58fb9176b54b2bdc512288f8
skd6150 <skd6150@gmail.com>
2022-05-14 00:39:04 +0900

    feat: interface 정의, recoil 적용

---
+++ interfaces/problem.ts
@@ -0,0 +1,7 @@
+export default interface problem {
+  prob_num: number;
+  prob_rank: number;
+  prob_link: string;
+  prob_name: string;
+  solved_date: string;
+}
---
+++ interfaces/user.ts
@@ -0,0 +1,8 @@
+import problem from "./problem";
+
+export default interface user {
+  user_name: string;
+  user_link: string;
+  exp: number;
+  solved_data: problem[];
+}
